00d1ef03941aaa061e6ff4da504e264171f0f617
Fellipe Oliveira <ofellipe.dev@gmail.com>
2023-05-27 16:25:03 -0300

    FIX = data database info config

--- Source/Support/Config-exemple.php
+++ Source/Support/Config-exemple.php
@@ -3,7 +3,7 @@
 /**
  * DATABASE
  */
-define ("CONF_DB_HOST", "localhost");
-define ("CONF_DB_NAME", "crud");
-define ("CONF_DB_USER", "root");
-define ("CONF_DB_PASS", "1812");
+define ("CONF_DB_HOST", "");
+define ("CONF_DB_NAME", "");
+define ("CONF_DB_USER", "");
+define ("CONF_DB_PASS", "");
